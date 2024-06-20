345613e97fc747c3cebfb7579f58b4da6b1b5e3d
Sunil-B-K <sunilkuligod21@gmail.com>
2024-06-20 13:29:32 +0530

    second commit

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.use("/images", express.static("upload/images"));
 app.post("/upload", upload.single("product"), (req, res) => {
   res.json({
     success: 1,
-    image_url: `https://e-backend-jkn8.onrender.com/images/${req.file.filename}`,
+    image_url: `https://startling-faloodeh-fd04b5.netlify.app/images/${req.file.filename}`,
   });
 });
 
